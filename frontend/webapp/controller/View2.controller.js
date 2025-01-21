1f853cb2746ea33f6d627dec33fa48d68162a415
Male Naga Ramakrishna <ramakrishna@inflexiontechfze.com>
2025-01-21 06:33:58 +0000

    pac new project from BTP

---
+++ frontend/webapp/controller/View2.controller.js
@@ -0,0 +1,24 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast"
+], function (Controller, MessageToast) {
+    "use strict";
+
+    return Controller.extend("app.dummy.controller.View1", {
+        onInit: function () {
+        },
+
+        onLoginPress: function () {
+            // Example validation logic for login
+            var username = this.byId("userInput").getValue();
+            var password = this.byId("passwordInput").getValue();
+
+            if (username === "admin" && password === "admin") {
+                MessageToast.show("Login successful");
+                this.getOwnerComponent().getRouter().navTo("view2");
+            } else {
+                MessageToast.show("Invalid credentials");
+            }
+        }
+    });
+});
